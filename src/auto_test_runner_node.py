b7bcce15c7b84f7ce4ceaa5cc8a798f3e0515f72
Pauline Mae Cuaresma <pauline.cuaresma@bionautlabs.com>
2023-10-19 14:30:36 -0700

    initial auto_test_runner_node skeleton

--- src/auto_test_runner_node.py
+++ src/auto_test_runner_node.py
@@ -1,5 +1,6 @@
 import rclpy
-from rclpy.callback_groups import CallbackGroup
+import signal
+from typing import Type
 from rclpy.node import Node
 from std_msgs.msg import String
 from enum import Enum
@@ -9,13 +10,19 @@ from enum import Enum
 
 QUEUE_SIZE = 10
 
+# or import these topics
 class PublisherTopics(Enum):
     PUB_TOP_1 = '/pub_top_1'
     PUB_TOP_2 = '/pub_top_2'
 
+
 class SubscriberTopics(Enum):
     SUB_TOP_1 = '/sub_top_1'
     SUB_TOP_2 = '/sub_top_2'
+
+
+class TestRunnerNodeException(Exception):
+    pass
 
 
 class AutoTestRunnerNode(Node):
@@ -37,7 +44,7 @@ class AutoTestRunnerNode(Node):
     def _callback(self, topic_name, msg=None):
         received_msg = msg.data
         # consider logging reeived message
-        self.get_logger().info('Received message topic, topic_name = %s' %topic_name)
+        self.logger.info('Received message topic, topic_name = %s' %topic_name)
 
         # for one of these handlers, if a mode is in waiting for confirm state then publish user confirm
         # log echoing which state procedure state is in
@@ -48,6 +55,43 @@ class AutoTestRunnerNode(Node):
                 self._handle_sub_top_2()
             case _:
                 self._handle_default()
+
+    def _handle_sub_top_1(self):
+        pass
+
+    def _handle_sub_top_2(self):
+        pass
+
+    def _handle_default(self):
+        pass
+
+
+def start_auto_test_runner_node(test_runner_node: Type[AutoTestRunnerNode], args=None) -> None:
+
+    rclpy.init(args=args)
+    node = test_runner_node()
+
+    def signal_handler(sig, frame):
+        node.logger.info('Received SIGINT. Terminating node.')
+        node.logger.info('Safely shutting down node.')
+        node.destroy_node()
+        rclpy.try_shutdown()
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    try:
+        rclpy.spin(node)
+    except TestRunnerNodeException as inst:
+        msg = inst.args
+        node.logger.error(f'Node terminating on exception: {msg}')
+        node.logger.info('Safely shutting down node.')
+        node.destroy_node()
+        rclpy.try_shutdown()
+
+
+def main(args=None):
+    start_auto_test_runner_node(AutoTestRunnerNode, args=args)
+
 
 
 
